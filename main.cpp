e86e820bb1e8eb11efbcc23084036ecbd4e9171f
Артем Щукин <a_r_shukin@mail.ru>
2023-02-08 13:53:53 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,33 +1,25 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-
 using namespace std;
 
 int main() {
-    printf("Введите набор целых чисел через пробел:");
-    string inputline;
-    getline(cin, inputline);
-    istringstream iss(inputline);
-
-    vector<int> values;
-    values.reserve((inputline.length() + 1)/2); // heuristics
-    int inputValue;
-    while (iss >> inputValue)
-        values.push_back(inputValue);
-
-    auto size = values.size();
+    int size;
+    cout<<"Сколько чисел будете вводить?: ";
+    cin>>size;
     int max = 0;
     int min = 0;
     int sum = 0;
-    for (auto v : values) {
-        if (v > max) {
-            max = v;
+    for (int i = 0; i < size; i++) {
+        int numberVal;
+        cin>>numberVal;
+        if (numberVal > max) {
+            max = numberVal;
         }
-        if (v < min || min == 0) {
-            min = v;
+        if (numberVal < min || min == 0) {
+            min = numberVal;
         }
-        sum += v;
+        sum += numberVal;
     }
     printf("Максимальное число: %i \n", max);
     printf("Минимальное число: %i \n", min);
